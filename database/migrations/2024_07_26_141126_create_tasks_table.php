d05507446f504993d3b00dd9073a0ee2046a8b01
Travel-Agent7 <127250295+Travel-Agent7@users.noreply.github.com>
2024-07-28 18:09:50 +0400

    Update 2024_07_26_141126_create_tasks_table.php

--- database/migrations/2024_07_26_141126_create_tasks_table.php
+++ database/migrations/2024_07_26_141126_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->foreignId('category_id');
-            $table->string('title');
+            $table->string('taskname');
             $table->text('description')->nullable();
             $table->date('start_date');
             $table->date('end_date');
